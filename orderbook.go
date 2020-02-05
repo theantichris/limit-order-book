413df0e5f1702bf1e863b118834be056a34b51f6
Christopher Lamm <chris@theantichris.com>
2020-02-05 09:10:41 -0500

    implemented adding an order to the book

--- orderbook.go
+++ orderbook.go
@@ -5,12 +5,13 @@ const maxPrice = 10000000
 // OrderStatus represents the status of an individual order.
 type OrderStatus int
 
+// The valid statuses of an order on the book.
 const (
-	new OrderStatus = iota
-	open
-	partial
-	filled
-	cancelled
+	OrderStatusNew OrderStatus = iota
+	OrderStatusOpen
+	OrderStatusPartial
+	OrderStatusFilled
+	OrderStatusCancelled
 )
 
 // Order represents an order on the book. Orders are either buy or sell.
@@ -20,6 +21,7 @@ type Order struct {
 	isBuy  bool
 	price  uint32
 	amount uint32
+	status OrderStatus
 	next   *Order
 }
 
@@ -30,10 +32,40 @@ type PricePoint struct {
 	orderTail *Order
 }
 
+// Insert adds an order to the current price point.
+func (pricePoint *PricePoint) Insert(order *Order) {
+	if pricePoint.orderHead == nil {
+		pricePoint.orderHead = order
+		pricePoint.orderTail = order
+	} else {
+		pricePoint.orderTail.next = order
+		pricePoint.orderTail = order
+	}
+}
+
 // OrderBook keeps track of the current maximum bid and minimum ask, the orders, and possible price points.
 type OrderBook struct {
 	ask        uint32
 	bid        uint32
 	orderIndex map[uint64]*Order
 	prices     [maxPrice]*PricePoint
+}
+
+// OpenOrder inserts a new order into the book.
+// It appends the order to the list of orders at its price point, updates the bid or ask, and adds an entry in the order index.
+func (orderBook *OrderBook) OpenOrder(order *Order) {
+	pricePoint := orderBook.prices[order.price]
+	pricePoint.Insert(order)
+
+	order.status = OrderStatusNew
+
+	if order.isBuy && order.price > orderBook.bid {
+		orderBook.bid = order.price
+	}
+
+	if !order.isBuy && order.price < orderBook.ask {
+		orderBook.ask = order.price
+	}
+
+	orderBook.orderIndex[order.id] = order
 }
